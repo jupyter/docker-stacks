4c34446dc75417faa416537e19712974ba5ea521
Peter Parente <pparent@us.ibm.com>
2015-09-22 20:50:15 -0400

    Fix jovyan hardcode in start-notebook.sh
    Contribution (c) Copyright IBM Corp. 2015

--- minimal-notebook/start-notebook.sh
+++ minimal-notebook/start-notebook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Change UID of jovyan to NB_UID if it does not match
-if [ "$NB_UID" != $(id -u jovyan) ] ; then
+# Change UID of NB_USER to NB_UID if it does not match
+if [ "$NB_UID" != $(id -u $NB_USER) ] ; then
     usermod -u $NB_UID $NB_USER
     chown -R $NB_UID $CONDA_DIR
 fi
