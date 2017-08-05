a17763d8340d9153192f19f58f13787c4185bade
BurdenBear <541795600@qq.com>
2017-08-05 11:09:04 +0800

    feat:    use conda system config

--- ipyparallel-notebook/systemuser.sh
+++ ipyparallel-notebook/systemuser.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-if getent passwd $USER_ID > /dev/null ; then
-  echo "$USER ($USER_ID) exists"
+if getent passwd ${USER_ID} > /dev/null ; then
+  echo "${USER} (${USER_ID}) exists"
 else
-  echo "Creating user $USER ($USER_ID)"
-  useradd -u $USER_ID -s $SHELL $USER
+  echo "Creating user ${USER} (${USER_ID})"
+  useradd -u ${USER_ID} -m ${HOME} -s $SHELL ${USER}
 fi
 
 notebook_arg=""
@@ -17,16 +17,24 @@ echo create ipython profile
 
 /tmp/profile/create_profile.sh -f /tmp/profile/profiles
 
-sudo chown -R ${USER} ~/.ipython
+sudo chown -R ${USER} ${HOME}/.ipython
 
-sudo -E PATH="${CONDA_DIR}/bin:$PATH" -E CONDA_ENVS_PATH="${HOME}/envs:${CONDA_DIR}/envs" \
-  -u $USER $CONDA_DIR/bin/jupyterhub-singleuser \
+# update pip source
+sudo -u ${USER} mkdir -p ${HOME}/.pip && \
+echo "[global]" > ${HOME}/.pip/pip.conf && \
+echo "index-url = http://mirrors.aliyun.com/pypi/simple/" >> ${HOME}/.pip/pip.conf && \
+echo "" >> ${HOME}/.pip/pip.conf && \
+echo "[install]" >> ${HOME}/.pip/pip.conf && \
+echo "trusted-host=mirrors.aliyun.com" >> ${HOME}/.pip/pip.conf
+
+sudo -E PATH="${CONDA_DIR}/bin:$PATH" -E CONDA_ENVS_PATH="${HOME}/.conda/envs" \
+  -u ${USER} ${CONDA_DIR}/bin/jupyterhub-singleuser \
   --port=8888 \
   --ip=0.0.0.0 \
-  --user=$JPY_USER \
-  --cookie-name=$JPY_COOKIE_NAME \
-  --base-url=$JPY_BASE_URL \
-  --hub-prefix=$JPY_HUB_PREFIX \
-  --hub-api-url=$JPY_HUB_API_URL \
+  --user=${JPY_USER} \
+  --cookie-name=${JPY_COOKIE_NAME} \
+  --base-url=${JPY_BASE_URL} \
+  --hub-prefix=${JPY_HUB_PREFIX} \
+  --hub-api-url=${JPY_HUB_API_URL} \
   ${notebook_arg} \
   $@
