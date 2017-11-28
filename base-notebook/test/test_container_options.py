6ba0d5944646ca327de4881b90270a252b2e8ac3
Peter Parente <parente@cs.unc.edu>
2017-11-27 23:28:11 -0500

    Add placeholder tests for base-notebook

---
+++ base-notebook/test/test_container_options.py
@@ -0,0 +1,43 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+import pytest
+
+
+@pytest.mark.skip('placeholder')
+def test_cli_args():
+    pass
+
+
+@pytest.mark.skip('placeholder')
+def test_unsigned_ssl():
+    pass
+
+@pytest.mark.skip('placeholder')
+def test_uid_change():
+    pass
+
+
+@pytest.mark.skip('placeholder')
+def test_gid_change():
+    pass
+
+
+@pytest.mark.skip('placeholder')
+def test_group_add():
+    pass
+
+
+@pytest.mark.skip('placeholder')
+def test_sudo():
+    pass
+
+
+@pytest.mark.skip('placeholder')
+def test_host_mount():
+    pass
+
+
+@pytest.mark.skip('placeholder')
+def test_alt_command():
+    pass
+
