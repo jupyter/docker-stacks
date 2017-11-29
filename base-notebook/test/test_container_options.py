8a59d74813cdda217729031284e481754b3a8c76
Peter Parente <parente@cs.unc.edu>
2017-11-28 23:57:05 -0500

    Refactor to share fixtures with option tests

--- base-notebook/test/test_container_options.py
+++ base-notebook/test/test_container_options.py
@@ -1,16 +1,38 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+import time
+
 import pytest
 
 
-@pytest.mark.skip('placeholder')
-def test_cli_args():
-    pass
+def test_cli_args(container, http_client):
+    """Container should respect notebook server command line args
+    (e.g., disabling token security)"""
+    container.run(
+        command=['start-notebook.sh', '--NotebookApp.token=""']
+    )
+    resp = http_client.get('http://localhost:8888')
+    resp.raise_for_status()
+    assert 'login_submit' not in resp.text
 
 
-@pytest.mark.skip('placeholder')
-def test_unsigned_ssl():
-    pass
+@pytest.mark.filterwarnings('ignore:Unverified HTTPS request')
+def test_unsigned_ssl(container, http_client):
+    """Container should generate a self-signed SSL certificate
+    and notebook server should use it to enable HTTPS.
+    """
+    c = container.run(
+        environment=['GEN_CERT=yes']
+    )
+    # NOTE: The requests.Session backing the http_client fixture  does not retry
+    # properly while the server is booting up. An SSL handshake error seems to
+    # abort the retry logic. Forcing a long sleep for the moment until I have
+    # time to dig more.
+    time.sleep(5)
+    resp = http_client.get('https://localhost:8888', verify=False)
+    resp.raise_for_status()
+    assert 'login_submit' in resp.text
+
 
 @pytest.mark.skip('placeholder')
 def test_uid_change():
--- test/test_notebook.py
+++ test/test_notebook.py
@@ -1,52 +1,9 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
-import os
-import time
 
-import docker
-import pytest
-import requests
-
-
-@pytest.fixture(scope='session')
-def docker_client():
-    """Docker client to use"""
-    return docker.from_env()
-
-
-@pytest.fixture(scope='session')
-def image_name():
-    """Image name to test"""
-    return os.getenv('TEST_IMAGE', 'jupyter/base-notebook')
-
-
-@pytest.fixture(scope='function')
-def nb_container(docker_client, image_name):
-    """Notebook container to test"""
-    container = docker_client.containers.run(
-        image_name,
-        detach=True,
-        auto_remove=True,
-        ports={
-            '8888/tcp': 8888
-        }
-    )
-    yield container
-    container.kill()
-
-
-def test_server_liveliness(nb_container):
-    """Notebook server should eventually respond with HTTP 200 OK."""
-    for i in range(10):
-        try:
-            resp = requests.get('http://localhost:8888')
-        except requests.exceptions.ConnectionError:
-            # Wait a bit and try again. Just because the docker container
-            # is running doesn't mean the notebook server is ready to accept
-            # connections inside it.
-            time.sleep(i)
-        else:
-            assert resp.status_code == 200
-            break
-    else:
-        assert False, 'could not connect to server'
+def test_secured_server(container, http_client):
+    """Notebook server should eventually request user login."""
+    container.run()
+    resp = http_client.get('http://localhost:8888')
+    resp.raise_for_status()
+    assert 'login_submit' in resp.text
