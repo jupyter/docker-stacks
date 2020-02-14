d1e10fc795a1fe5d1538e42541d868d53c1d7b03
romainx <cros.romain@gmail.com>
2020-02-14 20:07:18 +0100

    Remove pandoc os package install

---
+++ base-notebook/test/test_pandoc.py
@@ -0,0 +1,20 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+import logging
+
+import pytest
+
+LOGGER = logging.getLogger(__name__)
+
+
+def test_pandoc(container):
+    """Pandoc shall be able to convert MD to HTML."""
+    c = container.run(
+        tty=True, command=["start.sh", "bash", "-c", 'echo "**BOLD**" | pandoc']
+    )
+    c.wait(timeout=10)
+    logs = c.logs(stdout=True).decode("utf-8")
+    LOGGER.debug(logs)
+    assert "<p><strong>BOLD</strong></p>" in logs
+
