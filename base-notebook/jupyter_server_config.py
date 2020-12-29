c41616d7f30ed957f1fdf523819d328083270062
romainx <romainx@users.noreply.github.com>
2020-12-29 17:44:16 +0100

    #1205: Fix the configuration in jupyter_server_config.py

--- base-notebook/jupyter_server_config.py
+++ base-notebook/jupyter_server_config.py
@@ -8,9 +8,9 @@ import errno
 import stat
 
 c = get_config()  # noqa: F821
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
+c.ServerApp.ip = '0.0.0.0'
+c.ServerApp.port = 8888
+c.ServerApp.open_browser = False
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
