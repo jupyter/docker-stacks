9648e05b7c6ad95cbfd92ad6c4756bd63e52a712
romainx <romainx@users.noreply.github.com>
2020-12-29 17:51:44 +0100

    #1205: Fix c.ServerApp.certfile

--- base-notebook/jupyter_server_config.py
+++ base-notebook/jupyter_server_config.py
@@ -47,7 +47,7 @@ distinguished_name = req_distinguished_name
                            '-out', pem_file])
     # Restrict access to the file
     os.chmod(pem_file, stat.S_IRUSR | stat.S_IWUSR)
-    c.NotebookApp.certfile = pem_file
+    c.ServerApp.certfile = pem_file
 
 # Change default umask for all subprocesses of the notebook server if set in
 # the environment
