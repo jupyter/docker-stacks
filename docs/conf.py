ac969ae62ac2db639a714aaa308f46d8c8851646
Ayaz Salikhov <asalikhov@aimtech.team>
2021-04-26 13:30:34 +0300

    Update needs_sphinx variable

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.4'
+needs_sphinx = '2.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
