75493fa7c5f15ab88fddcf0204167662fa4700cc
Vladimir <vladimir.garanin@stfc.ac.uk>
2021-10-21 14:53:47 +0000

    Clean repo and make changes to grant sudo permissions

--- scipy-notebook/test/units/unit_pandas.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
-
-import numpy as np
-import pandas as pd
-
-
-np.random.seed(0)
-print(pd.Series(np.random.randint(0, 7, size=10)).sum())
