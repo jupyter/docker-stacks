897b6e054471e97d1fa1ef17f1f3726fcdf23e2d
Ayaz Salikhov <asalikhov@aimtech.team>
2022-01-22 19:43:47 +0300

    Better warnings check

--- pyspark-notebook/test/test_spark.py
+++ pyspark-notebook/test/test_spark.py
@@ -11,9 +11,14 @@ LOGGER = logging.getLogger(__name__)
 def test_spark_shell(container: TrackedContainer) -> None:
     """Checking if Spark (spark-shell) is running properly"""
     logs = container.run_and_wait(
-        no_warnings=False,  # WARNING: An illegal reflective access operation has occurred
         timeout=60,
+        no_warnings=False,
         tty=True,
         command=["start.sh", "bash", "-c", 'spark-shell <<< "1+1"'],
     )
+    # Spark warning
+    warnings = TrackedContainer.get_warnings(logs)
+    assert len(warnings) == 1
+    assert "An illegal reflective access operation has occurred" in warnings[0]
+
     assert "res0: Int = 2" in logs, "spark-shell does not work"
