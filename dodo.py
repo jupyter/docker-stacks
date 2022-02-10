2b99c910f6179c1dfa08edcbd46d044cba69d4fc
Tania Allard <taniar.allard@gmail.com>
2022-02-10 15:42:10 +0000

    :building_construction: Add Docker build to doit

--- dodo.py
+++ dodo.py
@@ -4,17 +4,24 @@ import doit
 import doit.tools
 
 # global doit config
-DOIT_CONFIG = {"verbosity": 2}
+DOIT_CONFIG = {"verbosity": 2, "default_tasks": ["build_docker"]}
+
+
+# dependencies: input to the task execution -> keeps tracks of the state of file dependencies and saves the signature of them every time the tasks are run so if there are no modifications to the files the execution of the task is skipped  (indicated by -- after running doit)
+# target: output produces by the task execution
+
+# -----------------------------------------------------------------------------
+# Documentation and wiki tasks
+# -----------------------------------------------------------------------------
 
 
 def task_docs():
-    """Build Sphinx documentation"""
+    """Build Sphinx documentation 📝"""
 
     yield dict(
-        name="Build Sphinx documentation",
+        name="docs:build",
         file_dep=[*P.DOCS_MD, *P.DOCS_RST, *P.DOCS_PY],
-        # actions=["sphinx-build -W docs/ docs/_build/html"],
-        actions=[f"sphinx-build -W {P.DOCS} docs/_build/html"],
+        actions=[U.do("sphinx-build", "-W", P.DOCS, "docs/_build/html")],
         targets=["_build/"],
         uptodate=[False],
     )
@@ -22,13 +29,54 @@ def task_docs():
 
 @doit.create_after("docs")
 def task_check():
-    """Perform checks of built doc artifacts"""
+    """Checks for any broken links in the Sphinx documentation 🔗"""
     yield dict(
         name="docs:links",
-        doc="Check for broken (internal) links",
-        file_dep=[*P.DOCS.rglob("*.html")],
-        actions=["sphinx-build -W --keep-going --color -b linkcheck docs/ docs/_build"],
+        file_dep=[*P.DOCS.rglob("_build/*.html")],
+        actions=[
+            U.do(
+                "sphinx-build",
+                "-W",
+                "--keep-going",
+                "--color",
+                "-b",
+                "linkcheck",
+                P.DOCS,
+                "docs/_build",
+            )
+        ],
     )
+
+
+# -----------------------------------------------------------------------------
+# Docker related
+# -----------------------------------------------------------------------------
+
+
+def task_build_docker():
+    """Build Docker images for system's architecture ⛏"""
+    for image in P.TEST_IMAGES:
+        tag = f"{P.OWNER}/{image}:latest"
+        dockerfile = P.ROOT / image / "Dockerfile"
+
+        yield dict(
+            name="(system's architecture): " + image,
+            actions=[
+                U.do(
+                    "docker",
+                    "buildx",
+                    "build",
+                    "-t",
+                    str(tag),
+                    str(P.ROOT / image),
+                    "--build-arg",
+                    "OWNER=" + P.OWNER,
+                )
+            ],
+            file_dep=[dockerfile],
+            targets=[image],
+            uptodate=[False],
+        )
 
 
 # -----------------------------------------------------------------------------
@@ -57,12 +105,28 @@ class P:
     # CI
     CI = ROOT / ".github"
 
-    # images
-    ALL_SPARK_NB = ROOT / "all-spark-notebook"
-    BASE_NB = ROOT / "base-notebook"
-    DATASCIENCE_NB = ROOT / "datascience-notebook"
-    MINIMAL_NB = ROOT / "minimal-notebook"
-    PYSPARK_NB = ROOT / "pyspark-notebook"
-    R_NB = ROOT / "r-notebook"
-    SCIPY_NB = ROOT / "scipy-notebook"
-    TENSORFLOW_NB = ROOT / "tensorflow-notebook"
+    # Images supporting the following architectures:
+    # - linux/amd64
+    # - linux/arm64
+    MULTI_IMAGES = [
+        "base-notebook",
+        "minimal-notebook",
+        "pyspark-notebook",
+        "r-notebook",
+        "scipy-notebook",
+        "all-spark-notebook",
+    ]
+    AMD64_IMAGES = ["datascience-notebook", "tensorflow-notebook"]
+    ALL_IMAGES = MULTI_IMAGES + AMD64_IMAGES
+    TEST_IMAGES = ["base-notebook"]
+
+    OWNER = "jupyter"
+
+
+class U:
+    """Supporting methods (a.k.a utilities)"""
+
+    @staticmethod
+    def do(*args, cwd=P.ROOT, **kwargs):
+        """wrap a CmdAction for consistency across OS"""
+        return doit.tools.CmdAction(list(args), shell=False, cwd=str(Path(cwd)))
