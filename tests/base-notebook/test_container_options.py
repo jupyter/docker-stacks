fe21bee2b7824efe6e2087515eee3cdc13ac4162
Islam Mansour <is3mansour@gmail.com>
2022-06-22 09:19:43 +0000

    update test case

--- tests/base-notebook/test_container_options.py
+++ tests/base-notebook/test_container_options.py
@@ -83,12 +83,11 @@ def test_gid_change(container: TrackedContainer) -> None:
 
 def test_additional_groups_change(container: TrackedContainer) -> None:
     """Container should add additional groups to the default user."""
-    nb_user_groups = "104(input),125(render),124(kvm),107(messagebus),450,150(test)"
     logs = container.run_and_wait(
         timeout=10,
         tty=True,
         user="root",
-        environment=[f"NB_UNPRIVILEGED_GROUPS={nb_user_groups}"],
+        environment=["NB_UNPRIVILEGED_GROUPS=104(input),125(render),124(kvm),107(messagebus),450,150(test)"],
         command=["start.sh", "id"],
     )
     assert (
