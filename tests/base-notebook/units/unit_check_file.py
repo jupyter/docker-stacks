7c4f3bd4a0228ab89908cabe6da95c53d3a4fa1d
Ayaz Salikhov <mathbunnyru@gmail.com>
2022-07-05 13:39:18 +0400

    Remove unit_check_file

--- tests/base-notebook/units/unit_check_file.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
-
-from pathlib import Path
-
-file = Path("/tmp/test-file.txt")
-assert file.exists()
-assert file.read_text() == "test-content\n"
