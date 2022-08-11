e620b105a388cfcc13f0f26ae40932aae69bd007
Ayaz Salikhov <mathbunnyru@gmail.com>
2022-08-12 00:39:51 +0400

    Use python pathlib where possible

--- base-notebook/jupyter_server_config.py
+++ base-notebook/jupyter_server_config.py
@@ -4,6 +4,7 @@
 import os
 import stat
 import subprocess
+from pathlib import Path
 
 from jupyter_core.paths import jupyter_data_dir
 
@@ -22,15 +23,14 @@ distinguished_name = req_distinguished_name
 [req_distinguished_name]
 """
 if "GEN_CERT" in os.environ:
-    dir_name = jupyter_data_dir()
-    pem_file = os.path.join(dir_name, "notebook.pem")
-    os.makedirs(dir_name, exist_ok=True)
+    jupyter_dir = Path(jupyter_data_dir())
+    jupyter_dir.mkdir(exist_ok=True)
+    pem_file = jupyter_dir / "notebook.pem"
 
     # Generate an openssl.cnf file to set the distinguished name
-    cnf_file = os.path.join(os.getenv("CONDA_DIR", "/usr/lib"), "ssl", "openssl.cnf")
-    if not os.path.isfile(cnf_file):
-        with open(cnf_file, "w") as fh:
-            fh.write(OPENSSL_CONFIG)
+    cnf_file = Path(os.getenv("CONDA_DIR", "/usr/lib")) / "ssl/openssl.cnf"
+    if not cnf_file.is_file():
+        cnf_file.write_text(OPENSSL_CONFIG)
 
     # Generate a certificate if one doesn't exist on disk
     subprocess.check_call(
@@ -48,7 +48,7 @@ if "GEN_CERT" in os.environ:
         ]
     )
     # Restrict access to the file
-    os.chmod(pem_file, stat.S_IRUSR | stat.S_IWUSR)
+    pem_file.chmod(stat.S_IRUSR | stat.S_IWUSR)
     c.ServerApp.certfile = pem_file
 
 # Change default umask for all subprocesses of the notebook server if set in
