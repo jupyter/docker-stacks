d4d054ca267a7b51794928e4dbc4813b1f8cc08b
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2022-08-20 00:17:19 +0400

    Update test_R_mimetypes.py

--- tests/r-notebook/test_R_mimetypes.py
+++ tests/r-notebook/test_R_mimetypes.py
@@ -2,10 +2,9 @@
 # Distributed under the terms of the Modified BSD License.
 
 from tests.conftest import TrackedContainer
-from tests.R_mimetype_check import mimetypes_check
+from tests.R_mimetype_check import check_r_mimetypes
 
 
-def test_mimetypes(container: TrackedContainer) -> None:
+def check_r_mimetypes(container: TrackedContainer) -> None:
     """Check if Rscript command for mimetypes can be executed"""
-    logs = mimetypes_check(container)
-    assert len(logs) == 0, "Command R command failed"
+    check_r_mimetypes(container)
