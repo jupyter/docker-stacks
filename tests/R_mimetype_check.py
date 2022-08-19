01c85d142daa16da657d4b376afb5b902da12f76
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2022-08-20 00:16:18 +0400

    Update R_mimetype_check.py

--- tests/R_mimetype_check.py
+++ tests/R_mimetype_check.py
@@ -8,16 +8,14 @@ from tests.conftest import TrackedContainer
 LOGGER = logging.getLogger(__name__)
 
 
-def mimetypes_check(container: TrackedContainer) -> str:
+def check_r_mimetypes(container: TrackedContainer) -> None:
     """Check if Rscript command can be executed"""
     LOGGER.info("Test that R command can be executed ...")
     Rcommand = 'if (length(getOption("jupyter.plot_mimetypes")) != 5) {stop("missing jupyter.plot_mimetypes")}'
     logs = container.run_and_wait(
-        timeout=60,
+        timeout=10,
         tty=True,
         command=["Rscript", "-e", Rcommand],
     )
     LOGGER.debug(f"{logs=}")
     assert len(logs) == 0, f"Command {Rcommand=} failed"
-
-    return logs
