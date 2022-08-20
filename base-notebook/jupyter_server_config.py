0fd68f0e6a84a01b4fda82dfc7cc0978bc7c6ec9
Darek <darek.bidek@gmail.com>
2022-08-20 02:17:03 -0400

    Remove inkscape package from the minimal-notebook (#1765)
    * Removing inkscape
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Adding Rprofile to add plot_mimetypes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update datascience-notebook/Dockerfile
    
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
    
    * Update datascience-notebook/Rprofile.site
    
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
    
    * Adding R notebook tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Moving Rprofile.site to minimal;
    Removing duplicate R test code
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixing flake8 errors
    
    * Update minimal-notebook/Dockerfile
    
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
    
    * Update minimal-notebook/Rprofile.site
    
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
    
    * Removing empty line
    
    * Renaming minetypes to  mimetypes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Renaming test module to avoid mypy error
    
    * Adding to jupyter.plot_mimetypes
    
    * Update R_mimetype_check.py
    
    * Update test_mimetypes.py
    
    * Update test_R_mimetypes.py
    
    * Update test_R_mimetypes.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>

--- base-notebook/jupyter_server_config.py
+++ base-notebook/jupyter_server_config.py
@@ -12,6 +12,9 @@ c.ServerApp.ip = "0.0.0.0"
 c.ServerApp.port = 8888
 c.ServerApp.open_browser = False
 
+# to output both image/svg+xml and application/pdf plot formats in the notebook file
+c.InlineBackend.figure_formats = {"png", "jpeg", "svg", "pdf"}
+
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
 
---
+++ tests/R_mimetype_check.py
@@ -0,0 +1,21 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+import logging
+
+from tests.conftest import TrackedContainer
+
+LOGGER = logging.getLogger(__name__)
+
+
+def check_r_mimetypes(container: TrackedContainer) -> None:
+    """Check if Rscript command can be executed"""
+    LOGGER.info("Test that R command can be executed ...")
+    Rcommand = 'if (length(getOption("jupyter.plot_mimetypes")) != 5) {stop("missing jupyter.plot_mimetypes")}'
+    logs = container.run_and_wait(
+        timeout=10,
+        tty=True,
+        command=["Rscript", "-e", Rcommand],
+    )
+    LOGGER.debug(f"{logs=}")
+    assert len(logs) == 0, f"Command {Rcommand=} failed"
