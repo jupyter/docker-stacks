61aa72ed82fa6fa4d4e3181ca03138fb47c1ce40
Kevin Bates <kbates4@gmail.com>
2022-11-05 09:14:35 -0700

    Split test_nb_user_change between the foundation and base tests

--- tests/base-notebook/test_container_options.py
+++ tests/base-notebook/test_container_options.py
@@ -42,30 +42,6 @@ def test_nb_user_change(container: TrackedContainer) -> None:
     # Give the chown time to complete.
     # Use sleep, not wait, because the container sleeps forever.
     time.sleep(1)
-    LOGGER.info(f"Checking if the user is changed to {nb_user} by the start script ...")
-    output = running_container.logs().decode("utf-8")
-    assert "ERROR" not in output
-    assert "WARNING" not in output
-    assert (
-        f"username: jovyan       -> {nb_user}" in output
-    ), f"User is not changed to {nb_user}"
-
-    LOGGER.info(f"Checking {nb_user} id ...")
-    command = "id"
-    expected_output = f"uid=1000({nb_user}) gid=100(users) groups=100(users)"
-    cmd = running_container.exec_run(command, user=nb_user, workdir=f"/home/{nb_user}")
-    output = cmd.output.decode("utf-8").strip("\n")
-    assert output == expected_output, f"Bad user {output}, expected {expected_output}"
-
-    LOGGER.info(f"Checking if {nb_user} owns his home folder ...")
-    command = f'stat -c "%U %G" /home/{nb_user}/'
-    expected_output = f"{nb_user} users"
-    cmd = running_container.exec_run(command, workdir=f"/home/{nb_user}")
-    output = cmd.output.decode("utf-8").strip("\n")
-    assert (
-        output == expected_output
-    ), f"Bad owner for the {nb_user} home folder {output}, expected {expected_output}"
-
     LOGGER.info(
         f"Checking if home folder of {nb_user} contains the hidden '.jupyter' folder with appropriate permissions ..."
     )
