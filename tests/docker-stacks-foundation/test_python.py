89975edf1720544ecf450ce71ed9cbf5bc06b39e
Bjørn Jørgensen <bjornjorgensen@gmail.com>
2022-12-08 15:33:21 +0100

    Update test_python.py

--- tests/docker-stacks-foundation/test_python.py
+++ tests/docker-stacks-foundation/test_python.py
@@ -5,7 +5,7 @@ import logging
 from tests.conftest import TrackedContainer
 
 LOGGER = logging.getLogger(__name__)
-EXPECTED_PYTHON_VERSION = "3.10"
+EXPECTED_PYTHON_VERSION = "3.11"
 
 
 def test_python_version(container: TrackedContainer) -> None:
