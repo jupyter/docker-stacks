24a014cfbd2e177c24546e3184d0570c282aa733
Ayaz Salikhov <mathbunnyru@gmail.com>
2022-12-29 23:33:14 +0200

    Test healthcheck properly

---
+++ tests/base-notebook/test_healthcheck.py
@@ -0,0 +1,18 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+import logging
+import time
+
+from tests.conftest import TrackedContainer, get_health
+
+LOGGER = logging.getLogger(__name__)
+
+
+def test_health(container: TrackedContainer) -> None:
+    running_container = container.run_detached(
+        tty=True,
+    )
+    # sleeping some time to let the server start
+    time.sleep(10)
+    assert get_health(running_container) == "healthy"
