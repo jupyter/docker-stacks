2fbc9b69c332e40e9f96d51598d71bb218989490
Ayaz Salikhov <mathbunnyru@gmail.com>
2023-01-03 03:57:27 +0300

    Check healthcheck in all jupyter applications

--- tests/base-notebook/test_healthcheck.py
+++ tests/base-notebook/test_healthcheck.py
@@ -3,15 +3,31 @@
 
 import logging
 import time
+from typing import Optional
+
+import pytest  # type: ignore
 
 from tests.conftest import TrackedContainer, get_health
 
 LOGGER = logging.getLogger(__name__)
 
 
-def test_health(container: TrackedContainer) -> None:
+@pytest.mark.parametrize(
+    "env",
+    [
+        None,
+        ["DOCKER_STACKS_JUPYTER_CMD=lab"],
+        ["RESTARTABLE=yes"],
+        ["DOCKER_STACKS_JUPYTER_CMD=notebook"],
+        ["DOCKER_STACKS_JUPYTER_CMD=server"],
+        ["DOCKER_STACKS_JUPYTER_CMD=nbclassic"],
+        ["JUPYTERHUB_API_TOKEN=my_token"],
+    ],
+)
+def test_health(container: TrackedContainer, env: Optional[list[str]]) -> None:
     running_container = container.run_detached(
         tty=True,
+        env=env,
     )
     # sleeping some time to let the server start
     time.sleep(15)
