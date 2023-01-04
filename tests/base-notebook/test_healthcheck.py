a905ff6f6d7b5e89e75ba603c9b4802a15b7d88f
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2023-01-04 10:24:09 +0400

    Check healthcheck in all jupyter applications (#1854)
    * Check healthcheck in all jupyter applications
    
    * Fix
    
    * Remove JUPYTERHUB_API_TOKEN healthcheck test

--- tests/base-notebook/test_healthcheck.py
+++ tests/base-notebook/test_healthcheck.py
@@ -3,15 +3,30 @@
 
 import logging
 import time
+from typing import Optional
+
+import pytest  # type: ignore
 
 from tests.conftest import TrackedContainer, get_health
 
 LOGGER = logging.getLogger(__name__)
 
 
-def test_health(container: TrackedContainer) -> None:
+@pytest.mark.parametrize(
+    "env",
+    [
+        None,
+        ["DOCKER_STACKS_JUPYTER_CMD=lab"],
+        ["RESTARTABLE=yes"],
+        ["DOCKER_STACKS_JUPYTER_CMD=notebook"],
+        ["DOCKER_STACKS_JUPYTER_CMD=server"],
+        ["DOCKER_STACKS_JUPYTER_CMD=nbclassic"],
+    ],
+)
+def test_health(container: TrackedContainer, env: Optional[list[str]]) -> None:
     running_container = container.run_detached(
         tty=True,
+        environment=env,
     )
     # sleeping some time to let the server start
     time.sleep(15)
