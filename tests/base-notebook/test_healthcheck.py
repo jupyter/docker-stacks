bfbc17a0513585e0e5dae59f8e38891864ea3c49
imiro <ajim1417@gmail.com>
2023-02-18 08:37:53 -0600

    Remove workdir from tests

--- tests/base-notebook/test_healthcheck.py
+++ tests/base-notebook/test_healthcheck.py
@@ -13,34 +13,31 @@ LOGGER = logging.getLogger(__name__)
 
 
 @pytest.mark.parametrize(
-    "env,cmd,workdir",
+    "env,cmd",
     [
-        (None, None, None),
-        (["DOCKER_STACKS_JUPYTER_CMD=lab"], None, None),
-        (["DOCKER_STACKS_JUPYTER_CMD=notebook"], None, None),
-        (["DOCKER_STACKS_JUPYTER_CMD=server"], None, None),
-        (["DOCKER_STACKS_JUPYTER_CMD=nbclassic"], None, None),
-        (["RESTARTABLE=yes"], None, None),
-        (["JUPYTER_PORT=8171"], None, None),
-        (["JUPYTER_PORT=8117", "DOCKER_STACKS_JUPYTER_CMD=notebook"], None, None),
-        (None, ["start-notebook.sh", "--NotebookApp.base_url=/test"], None),
-        (None, ["start-notebook.sh", "--NotebookApp.base_url=/test/"], None),
-        (["GEN_CERT=1"], ["start-notebook.sh", "--NotebookApp.base_url=/test"], None),
+        (None, None),
+        (["DOCKER_STACKS_JUPYTER_CMD=lab"], None),
+        (["DOCKER_STACKS_JUPYTER_CMD=notebook"], None),
+        (["DOCKER_STACKS_JUPYTER_CMD=server"], None),
+        (["DOCKER_STACKS_JUPYTER_CMD=nbclassic"], None),
+        (["RESTARTABLE=yes"], None),
+        (["JUPYTER_PORT=8171"], None),
+        (["JUPYTER_PORT=8117", "DOCKER_STACKS_JUPYTER_CMD=notebook"], None),
+        (None, ["start-notebook.sh", "--NotebookApp.base_url=/test"]),
+        (None, ["start-notebook.sh", "--NotebookApp.base_url=/test/"]),
+        (["GEN_CERT=1"], ["start-notebook.sh", "--NotebookApp.base_url=/test"]),
         (
             ["GEN_CERT=1", "JUPYTER_PORT=7891"],
             ["start-notebook.sh", "--NotebookApp.base_url=/test"],
-            None,
         ),
-        (["NB_USER=testuser", "CHOWN_HOME=1"], None, "/home/testuser"),
+        (["NB_USER=testuser", "CHOWN_HOME=1"], None),
         (
             ["NB_USER=testuser", "CHOWN_HOME=1"],
             ["start-notebook.sh", "--NotebookApp.base_url=/test"],
-            "/home/testuser",
         ),
         (
             ["NB_USER=testuser", "CHOWN_HOME=1", "JUPYTER_PORT=8123"],
             ["start-notebook.sh", "--NotebookApp.base_url=/test"],
-            "/home/testuser",
         ),
     ],
 )
@@ -48,13 +45,11 @@ def test_health(
     container: TrackedContainer,
     env: Optional[list[str]],
     cmd: Optional[list[str]],
-    workdir: Optional[str],
 ) -> None:
     running_container = container.run_detached(
         tty=True,
         environment=env,
         command=cmd,
-        working_dir=workdir,
     )
     # sleeping some time to let the server start
     time.sleep(15)
