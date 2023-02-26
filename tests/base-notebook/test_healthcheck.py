038ccd06285305edaf1f2389e861ee566b4ac3ce
imiro <ajim1417@gmail.com>
2023-02-26 15:53:44 -0600

    Update healthcheck test
    - Add "user" parameter
    - Add tests when container should not be healthy

--- tests/base-notebook/test_healthcheck.py
+++ tests/base-notebook/test_healthcheck.py
@@ -13,36 +13,34 @@ LOGGER = logging.getLogger(__name__)
 
 
 @pytest.mark.parametrize(
-    "env,cmd",
+    "env,cmd,user",
     [
-        (None, None),
-        (["DOCKER_STACKS_JUPYTER_CMD=lab"], None),
-        (["DOCKER_STACKS_JUPYTER_CMD=notebook"], None),
-        (["DOCKER_STACKS_JUPYTER_CMD=server"], None),
-        (["DOCKER_STACKS_JUPYTER_CMD=nbclassic"], None),
-        (["RESTARTABLE=yes"], None),
-        (["JUPYTER_PORT=8171"], None),
-        (["JUPYTER_PORT=8117", "DOCKER_STACKS_JUPYTER_CMD=notebook"], None),
-        (None, ["start-notebook.sh", "--NotebookApp.base_url=/test"]),
-        (None, ["start-notebook.sh", "--NotebookApp.base_url=/test/"]),
-        (["GEN_CERT=1"], ["start-notebook.sh", "--NotebookApp.base_url=/test"]),
+        (None, None, None),
+        (["DOCKER_SfACKS_JUPYTER_CMD=lab"], None, None),
+        (["DOCKER_STACKS_JUPYTER_CMD=notebook"], None, None),
+        (["DOCKER_STACKS_JUPYTER_CMD=server"], None, None),
+        (["DOCKER_STACKS_JUPYTER_CMD=nbclassic"], None, None),
+        (["RESTARTABLE=yes"], None, None),
+        (["JUPYTER_PORT=8171"], None, None),
+        (["JUPYTER_PORT=8117", "DOCKER_STACKS_JUPYTER_CMD=notebook"], None, None),
+        (None, ["start-notebook.sh", "--NotebookApp.base_url=/test"], None),
+        (None, ["start-notebook.sh", "--NotebookApp.base_url=/test/"], None),
+        (["GEN_CERT=1"], ["start-notebook.sh", "--NotebookApp.base_url=/test"], None),
         (
             ["GEN_CERT=1", "JUPYTER_PORT=7891"],
             ["start-notebook.sh", "--NotebookApp.base_url=/test"],
+            None,
         ),
-        (["NB_USER=testuser", "CHOWN_HOME=1"], None),
-        (["NB_USER='test user'", "CHOWN_HOME=1"], None),
+        (["NB_USER=testuser", "CHOWN_HOME=1"], None, "root"),
         (
             ["NB_USER=testuser", "CHOWN_HOME=1"],
             ["start-notebook.sh", "--NotebookApp.base_url=/test"],
-        ),
-        (
-            ["NB_USER='test user'", "CHOWN_HOME=1"],
-            ["start-notebook.sh", "--NotebookApp.base_url=/test"],
+            "root",
         ),
         (
             ["NB_USER=testuser", "CHOWN_HOME=1", "JUPYTER_PORT=8123"],
             ["start-notebook.sh", "--NotebookApp.base_url=/test"],
+            "root",
         ),
     ],
 )
@@ -50,11 +48,13 @@ def test_health(
     container: TrackedContainer,
     env: Optional[list[str]],
     cmd: Optional[list[str]],
+    user: Optional[str],
 ) -> None:
     running_container = container.run_detached(
         tty=True,
         environment=env,
         command=cmd,
+        user=user,
     )
 
     # sleeping some time to let the server start
@@ -68,3 +68,51 @@ def test_health(
             return
 
     assert get_health(running_container) == "healthy"
+
+
+@pytest.mark.parametrize(
+    "env,cmd,user",
+    [
+        (["NB_USER=testuser", "CHOWN_HOME=1"], None, None),
+        (["NB_USER='test user'", "CHOWN_HOME=1"], None, "root"),
+        (
+            ["NB_USER=testuser", "CHOWN_HOME=1"],
+            ["start-notebook.sh", "--NotebookApp.base_url=/test"],
+            None,
+        ),
+        (
+            ["NB_USER='test user'", "CHOWN_HOME=1"],
+            ["start-notebook.sh", "--NotebookApp.base_url=/test"],
+            "root",
+        ),
+        (
+            ["NB_USER=testuser", "CHOWN_HOME=1", "JUPYTER_PORT=8123"],
+            ["start-notebook.sh", "--NotebookApp.base_url=/test"],
+            None,
+        ),
+    ],
+)
+def test_not_healthy(
+    container: TrackedContainer,
+    env: Optional[list[str]],
+    cmd: Optional[list[str]],
+    user: Optional[str],
+) -> None:
+    running_container = container.run_detached(
+        tty=True,
+        environment=env,
+        command=cmd,
+        user=user,
+    )
+
+    # sleeping some time to let the server start
+    time_spent = 0.0
+    wait_time = 0.1
+    time_limit = 15
+    while time_spent < time_limit:
+        time.sleep(wait_time)
+        time_spent += wait_time
+        if get_health(running_container) == "healthy":
+            raise RuntimeError("Container should not be healthy for these testcases.")
+
+    assert get_health(running_container) != "healthy"
