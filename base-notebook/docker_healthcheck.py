2ec126ba005b23faa5145480cd1e409ccb3cf9f9
imiro <ajim1417@gmail.com>
2023-02-26 15:51:09 -0600

    Don't verify SSL certificate on healthcheck

--- base-notebook/docker_healthcheck.py
+++ base-notebook/docker_healthcheck.py
@@ -13,6 +13,6 @@ json_file = [s for s in file_list if s.endswith(".json")]
 url = json.load(open(runtime_dir + json_file[0]))["url"]
 url = url + "api"
 
-r = requests.get(url)
+r = requests.get(url, verify=False)  # request without SSL verification
 r.raise_for_status()
 print(r.content)
