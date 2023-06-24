0f5100d8fbc89cfc258b5f1bff6b7df806f7dd7d
Bjørn Jørgensen <bjornjorgensen@gmail.com>
2023-06-24 20:30:24 +0200

    move test to file

--- tests/pyspark-notebook/test_spark.py
+++ tests/pyspark-notebook/test_spark.py
@@ -16,20 +16,3 @@ def test_spark_shell(container: TrackedContainer) -> None:
     )
 
     assert "res0: Int = 2" in logs, "spark-shell does not work"
-
-
-def test_pandas_version(container: TrackedContainer) -> None:
-    """Checking if pandas is installed and its version is 1.5.3
-    This is only needed for Spark before 4.0 see #1924"""
-    logs = container.run_and_wait(
-        timeout=60,
-        tty=True,
-        command=[
-            "start.sh",
-            "python",
-            "-c",
-            "import pandas; print(pandas.__version__)",
-        ],
-    )
-
-    assert "1.5.3" in logs, "pandas version is not 1.5.3"
