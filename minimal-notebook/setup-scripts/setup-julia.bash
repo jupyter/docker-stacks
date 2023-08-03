e1bd309263e542f68187a5d89c73c09fa599faa3
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2023-08-03 13:18:01 +0400

    Use curl instead of wget for Spark and Julia downloads (#1950)
    Co-authored-by: Bjørn Jørgensen <bjornjorgensen@users.noreply.github.com>

--- minimal-notebook/setup-scripts/setup-julia.bash
+++ minimal-notebook/setup-scripts/setup-julia.bash
@@ -22,7 +22,8 @@ JULIA_MAJOR_MINOR=$(echo "${JULIA_VERSION}" | cut -d. -f 1,2)
 # Download and install Julia
 cd /tmp
 mkdir "/opt/julia-${JULIA_VERSION}"
-wget --progress=dot:giga "https://julialang-s3.julialang.org/bin/linux/${JULIA_SHORT_ARCH}/${JULIA_MAJOR_MINOR}/${JULIA_INSTALLER}"
+curl --progress-bar --location --output "${JULIA_INSTALLER}" \
+    "https://julialang-s3.julialang.org/bin/linux/${JULIA_SHORT_ARCH}/${JULIA_MAJOR_MINOR}/${JULIA_INSTALLER}"
 tar xzf "${JULIA_INSTALLER}" -C "/opt/julia-${JULIA_VERSION}" --strip-components=1
 rm "${JULIA_INSTALLER}"
 
