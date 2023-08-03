0ba7a4c15aab9df6812a0de1139c4981a2a8b6c3
Ayaz Salikhov <mathbunnyru@gmail.com>
2023-08-03 15:43:38 +0400

    Install latest docker version to aarch64 runners

--- aarch64-runner/setup.sh
+++ aarch64-runner/setup.sh
@@ -22,6 +22,16 @@ apt-get install --yes --no-install-recommends python3
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3
 
 echo "Setting up docker"
-apt-get install --yes --no-install-recommends docker.io
+apt-get remove --yes docker.io docker-doc docker-compose podman-docker containerd runc
+apt-get update
+apt-get install --yes ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install --yes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 usermod -aG docker ${GITHUB_RUNNER_USER}
 chmod 666 /var/run/docker.sock
