5e0752d5686fc8b4afd5636b3a5d66d40e5dd324
Ayaz Salikhov <mathbunnyru@gmail.com>
2023-08-18 20:04:57 +0200

    Add missing file

---
+++ docs/using/recipe_code/generate_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+import json
+from pathlib import Path
+
+THIS_DIR = Path(__file__).parent.resolve()
+ROOT_DIR = THIS_DIR.parent.parent
+
+
+def generate_matrix():
+    dockerfiles = sorted(file.name for file in THIS_DIR.glob("*.dockerfile"))
+    return {"dockerfile": dockerfiles}
+
+
+if __name__ == "__main__":
+    print(json.dumps(generate_matrix()))
