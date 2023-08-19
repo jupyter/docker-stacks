90cffa6c26f39cf62550fc5c0ec9da2185fdf67c
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2023-08-19 10:23:11 +0200

    Build all contributed recipes automatically (#1973)
    * Build all contributed recipes automatically
    
    * Add missing file
    
    * Add checkout repo
    
    * Fix matrix
    
    * Remove unused var
    
    * Add typing

---
+++ docs/using/recipe_code/generate_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+import json
+from pathlib import Path
+from typing import Any
+
+THIS_DIR = Path(__file__).parent.resolve()
+
+
+def generate_matrix() -> dict[str, Any]:
+    dockerfiles = sorted(file.name for file in THIS_DIR.glob("*.dockerfile"))
+    return {"dockerfile": dockerfiles}
+
+
+if __name__ == "__main__":
+    print("matrix=" + json.dumps(generate_matrix()))
