a5b40a6f1117bd675565b3673e063125dd74eac3
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2023-08-19 17:25:20 +0200

    Move all images to images dir (#1972)

--- tagging/write_manifest.py
+++ tagging/write_manifest.py
@@ -38,7 +38,7 @@ def write_build_history_line(
     links_column = MARKDOWN_LINE_BREAK.join(
         [
             f"[Git diff](https://github.com/jupyter/docker-stacks/commit/{commit_hash})",
-            f"[Dockerfile](https://github.com/jupyter/docker-stacks/blob/{commit_hash}/{short_image_name}/Dockerfile)",
+            f"[Dockerfile](https://github.com/jupyter/docker-stacks/blob/{commit_hash}/images/{short_image_name}/Dockerfile)",
             f"[Build manifest](./{filename})",
         ]
     )
