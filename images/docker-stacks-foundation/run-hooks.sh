1a22aa395872698c4f3e783bc3d8aae4a58e32d2
YuviPanda <yuvipanda@gmail.com>
2023-10-21 15:45:34 +0530

    Add warning to run-hooks.sh

--- images/docker-stacks-foundation/run-hooks.sh
+++ images/docker-stacks-foundation/run-hooks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+echo "WARNING: Use run-hooks.py instead"
 
 exec /usr/local/bin/run-hooks.py "$@"
