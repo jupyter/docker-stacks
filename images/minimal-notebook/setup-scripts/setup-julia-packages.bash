c205c106eb02759c9f4be41a94ac9c3ac557a3da
YuviPanda <yuvipanda@gmail.com>
2023-11-29 12:08:48 -0800

    Re-add export for JULIA_CPU_TARGET
    Quietens pre-commit

--- images/minimal-notebook/setup-scripts/setup-julia-packages.bash
+++ images/minimal-notebook/setup-scripts/setup-julia-packages.bash
@@ -20,11 +20,11 @@ set -exuo pipefail
 if [ "$(uname -m)" == "x86_64" ]; then
     # See https://github.com/JuliaCI/julia-buildkite/blob/70bde73f6cb17d4381b62236fc2d96b1c7acbba7/utilities/build_envs.sh#L24
     # for an explanation of these options
-    JULIA_CPU_TARGET="generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1)"
+    export JULIA_CPU_TARGET="generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1)"
 elif [ "$(uname -m)" == "aarch64" ]; then
     # See https://github.com/JuliaCI/julia-buildkite/blob/70bde73f6cb17d4381b62236fc2d96b1c7acbba7/utilities/build_envs.sh#L54
     # for an explanation of these options
-    JULIA_CPU_TARGET="generic;cortex-a57;thunderx2t99;carmel"
+    export JULIA_CPU_TARGET="generic;cortex-a57;thunderx2t99;carmel"
 fi
 
 # Install base Julia packages
