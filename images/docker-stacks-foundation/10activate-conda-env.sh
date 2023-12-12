74eba50de22be00cb97adf53c7800af837b59381
Ayaz Salikhov <mathbunnyru@gmail.com>
2023-12-13 00:08:23 +0400

    Fix typo

--- images/docker-stacks-foundation/10activate-conda-env.sh
+++ images/docker-stacks-foundation/10activate-conda-env.sh
@@ -5,7 +5,7 @@
 # This registers the initialization code for the conda shell code
 # It also activates default environment in the end, so we don't need to activate it manually
 # Documentation: https://docs.conda.io/projects/conda/en/latest/dev-guide/deep-dives/activation.html
-# Conda hook activates `base`` environment by default
+# Conda hook activates `base` environment by default
 # Instead, we activate "${DOCKER_STACKS_CONDA_ENV}" by default
 # This is done to provide cleaner environment when using custom conda environments
 eval "$(conda shell.bash hook | sed 's/conda activate base//g')"
