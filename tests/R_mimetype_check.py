3645a06d7b2d415ae0ef008420bb27d7c29052f1
Simon Li <orpheus+devel@gmail.com>
2024-01-21 21:54:24 +0000

    check_r_mimetypes: take account of logs from start.sh

--- tests/R_mimetype_check.py
+++ tests/R_mimetype_check.py
@@ -11,10 +11,14 @@ def check_r_mimetypes(container: TrackedContainer) -> None:
     """Check if Rscript command can be executed"""
     LOGGER.info("Test that R command can be executed ...")
     R_MIMETYPES_CHECK_CMD = 'if (length(getOption("jupyter.plot_mimetypes")) != 5) {stop("missing jupyter.plot_mimetypes")}'
+    command = ["Rscript", "-e", R_MIMETYPES_CHECK_CMD]
     logs = container.run_and_wait(
         timeout=10,
         tty=True,
-        command=["Rscript", "-e", R_MIMETYPES_CHECK_CMD],
+        command=command,
     )
     LOGGER.debug(f"{logs=}")
-    assert len(logs) == 0, f"Command {R_MIMETYPES_CHECK_CMD=} failed"
+    # If there is any output after this it means there was an error
+    assert logs.splitlines()[-1] == "Executing the command: " + " ".join(
+        command
+    ), f"Command {R_MIMETYPES_CHECK_CMD=} failed"
