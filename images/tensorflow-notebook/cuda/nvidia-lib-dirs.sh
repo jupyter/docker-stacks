b9553a8e5d33a8d59eac52b0d8790d3f46f6f03c
ChristofKaufmann <christofkaufmann.dev@gmail.com>
2024-03-26 02:12:59 +0100

    Add cuda12 variant of tensorflow-notebook (#2100)
    * Add cuda12 variant for tensorflow-notebook
    
    * Reduce size of CPU version of tensorflow-notebook
    
    * Try to fix tests
    
    * Update docs/using/selecting.md
    
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
    
    * Update images/tensorflow-notebook/cuda12/Dockerfile
    
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
    
    * Update tests/docker-stacks-foundation/test_packages.py
    
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
    
    * Remove obsolete XLA_FLAGS env var
    
    * Install CUDA and cuDNN using pip instead of mamba
    
    * Fix pre-commit shell checks
    
    * Change tensorflow variant name from cuda12 to cuda
    
    * Update selecting.md
    
    * Update selecting.md
    
    ---------
    
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>

---
+++ images/tensorflow-notebook/cuda/nvidia-lib-dirs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+# This adds the NVIDIA libraries to the LD_LIBRARY_PATH. Workaround for
+# https://github.com/tensorflow/tensorflow/issues/63362
+NVIDIA_DIR=$(dirname "$(python -c 'import nvidia;print(nvidia.__file__)')")
+LD_LIBRARY_PATH=$(echo "${NVIDIA_DIR}"/*/lib/ | sed -r 's/\s+/:/g')${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH
