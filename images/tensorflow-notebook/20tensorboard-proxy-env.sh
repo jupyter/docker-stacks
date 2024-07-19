014fa60b1e8f161bfb45271db71f35c08ce0597c
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2024-07-19 22:25:13 +0400

    Improve style

--- images/tensorflow-notebook/20tensorboard-proxy-env.sh
+++ images/tensorflow-notebook/20tensorboard-proxy-env.sh
@@ -5,6 +5,6 @@
 set -e
 
 # Initialize the TENSORBOARD_PROXY_URL with the appropriate path
-# to use jupyter-server-proxy .
+# to use jupyter-server-proxy.
 
 export TENSORBOARD_PROXY_URL="${JUPYTERHUB_SERVICE_PREFIX:-/}proxy/%PORT%/"
