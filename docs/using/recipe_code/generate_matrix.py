e136ad7574797c46940902cbe739c6165942f02b
Ayaz Salikhov <mathbunnyru@gmail.com>
2024-08-31 22:32:23 +0100

    Build contributed recipes in mathbunnyru fork as well

--- docs/using/recipe_code/generate_matrix.py
+++ docs/using/recipe_code/generate_matrix.py
@@ -2,17 +2,22 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 import json
+import os
 from pathlib import Path
 from typing import Any
 
 THIS_DIR = Path(__file__).parent.resolve()
+REPOSITORY_OWNER = os.environ["REPOSITORY_OWNER"]
 
 
 def generate_matrix() -> dict[str, Any]:
     dockerfiles = sorted(file.name for file in THIS_DIR.glob("*.dockerfile"))
+    runs_on = ["ubuntu-latest"]
+    if REPOSITORY_OWNER == "jupyter":
+        runs_on.append("ARM64")
     return {
         "dockerfile": dockerfiles,
-        "runs-on": ["ubuntu-latest", "ARM64"],
+        "runs-on": runs_on,
         "exclude": [{"dockerfile": "oracledb.dockerfile", "runs-on": "ARM64"}],
     }
 
