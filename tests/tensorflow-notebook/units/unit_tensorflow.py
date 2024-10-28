a1265be650bff7d1d38f0f2f68c10e32c369aedf
Ayaz Salikhov <mathbunnyru@gmail.com>
2024-10-28 12:23:29 +0000

    Get rid of abseil warnings

--- tests/tensorflow-notebook/units/unit_tensorflow.py
+++ tests/tensorflow-notebook/units/unit_tensorflow.py
@@ -1,6 +1,13 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+import os
+
 import tensorflow as tf
+
+# Suppress logging warnings
+# https://stackoverflow.com/a/78803598
+os.environ["GRPC_VERBOSITY"] = "ERROR"
+os.environ["GLOG_minloglevel"] = "2"
 
 print(tf.constant("Hello, TensorFlow"))
 print(tf.reduce_sum(tf.random.normal([1000, 1000])))
