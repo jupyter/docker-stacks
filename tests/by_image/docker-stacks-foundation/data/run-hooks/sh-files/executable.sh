0d18318d4cceaa571e5e3aa2496425c8b7ba3f75
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-03-03 16:35:28 +0000

    Add pre-commit hooks to check shebangs

--- tests/by_image/docker-stacks-foundation/data/run-hooks/sh-files/executable.sh
+++ tests/by_image/docker-stacks-foundation/data/run-hooks/sh-files/executable.sh
@@ -2,5 +2,5 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-export MY_VAR=123
-echo "Inside a.sh MY_VAR variable has ${MY_VAR} value"
+export MY_VAR=0
+echo "Inside executable.sh MY_VAR variable has ${MY_VAR} value"
