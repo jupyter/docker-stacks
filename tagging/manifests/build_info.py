31207df7ef89f0680c2cf17c91a9eda225afaaac
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-03-12 14:59:21 +0000

    Make taggers and manifests functions

--- tagging/manifests/build_info.py
+++ tagging/manifests/build_info.py
@@ -25,36 +25,33 @@ class BuildInfoConfig:
         return f"{self.registry}/{self.owner}/{self.image}"
 
 
-class BuildInfo:
+def build_info_manifest(config: BuildInfoConfig) -> MarkdownPiece:
     """BuildInfo doesn't fall under common interface, and we run it separately"""
+    commit_hash = GitHelper.commit_hash()
+    commit_hash_tag = GitHelper.commit_hash_tag()
+    commit_message = GitHelper.commit_message()
 
-    @staticmethod
-    def markdown_piece(config: BuildInfoConfig) -> MarkdownPiece:
-        commit_hash = GitHelper.commit_hash()
-        commit_hash_tag = GitHelper.commit_hash_tag()
-        commit_message = GitHelper.commit_message()
+    # Unfortunately, `docker images` doesn't work when specifying `docker.io` as registry
+    fixed_registry = config.registry + "/" if config.registry != "docker.io" else ""
 
-        # Unfortunately, `docker images` doesn't work when specifying `docker.io` as registry
-        fixed_registry = config.registry + "/" if config.registry != "docker.io" else ""
+    image_size = docker[
+        "images",
+        f"{fixed_registry}{config.owner}/{config.image}:latest",
+        "--format",
+        "{{.Size}}",
+    ]().rstrip()
 
-        image_size = docker[
-            "images",
-            f"{fixed_registry}{config.owner}/{config.image}:latest",
-            "--format",
-            "{{.Size}}",
-        ]().rstrip()
+    build_info = textwrap.dedent(
+        f"""\
+        - Build timestamp: {config.build_timestamp}
+        - Docker image: `{config.full_image()}:{commit_hash_tag}`
+        - Docker image size: {image_size}
+        - Git commit SHA: [{commit_hash}](https://github.com/{config.repository}/commit/{commit_hash})
+        - Git commit message:
 
-        build_info = textwrap.dedent(
-            f"""\
-            - Build timestamp: {config.build_timestamp}
-            - Docker image: `{config.full_image()}:{commit_hash_tag}`
-            - Docker image size: {image_size}
-            - Git commit SHA: [{commit_hash}](https://github.com/{config.repository}/commit/{commit_hash})
-            - Git commit message:
+        ```text
+        {{message}}
+        ```"""
+    ).format(message=commit_message)
 
-            ```text
-            {{message}}
-            ```"""
-        ).format(message=commit_message)
-
-        return MarkdownPiece(title="## Build Info", sections=[build_info])
+    return MarkdownPiece(title="## Build Info", sections=[build_info])
