329bf692dbe17875f6a0f185db54daf628cd7773
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-03-20 15:10:56 +0000

    Check requests

--- tests/by_image/base-notebook/data/check_listening.py
+++ tests/by_image/base-notebook/data/check_listening.py
@@ -7,17 +7,22 @@ import time
 import requests
 
 
-def test_connect() -> None:
+def make_get_request() -> None:
     # Give some time for server to start
     finish_time = time.time() + 10
     sleep_time = 1
     while time.time() < finish_time:
         time.sleep(sleep_time)
         try:
-            requests.get("http://localhost:8888/api")
-            break
+            resp = requests.get("http://localhost:8888/api")
+            resp.raise_for_status()
         except requests.RequestException:
             pass
+    resp.raise_for_status()
+
+
+def test_connect() -> None:
+    make_get_request()
 
     # https://docs.python.org/3/library/socket.html#socket.getaddrinfo
     ipv4_addrs = {
