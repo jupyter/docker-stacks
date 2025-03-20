8df7eab152e42127993f6e5f2209246af98a5632
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-03-20 16:27:32 +0000

    Use .api

--- tests/utils/get_container_health.py
+++ tests/utils/get_container_health.py
@@ -5,5 +5,5 @@ from docker.models.containers import Container
 
 
 def get_health(container: Container, client: docker.DockerClient) -> str:
-    inspect_results = client.inspect_container(container.name)
+    inspect_results = client.api.inspect_container(container.name)
     return inspect_results["State"]["Health"]["Status"]  # type: ignore
