ac3dbd5fa345a76fd4778516cb1dd786796d297f
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-03-20 16:59:18 +0000

    Rewrite check_listening.py to use one function for both ipv4 and ipv6

--- tests/by_image/base-notebook/data/check_listening.py
+++ tests/by_image/base-notebook/data/check_listening.py
@@ -21,35 +21,36 @@ def make_get_request() -> None:
     resp.raise_for_status()
 
 
+def check_addrs(family: socket.AddressFamily) -> None:
+    assert family in {socket.AF_INET, socket.AF_INET6}
+
+    # https://docs.python.org/3/library/socket.html#socket.getaddrinfo
+    addrs = {
+        s[4][0]
+        for s in socket.getaddrinfo(host=socket.gethostname(), port=None, family=family)
+    }
+    loopback_addr = "127.0.0.1" if family == socket.AF_INET else "::1"
+    addrs.discard(loopback_addr)
+
+    assert addrs, f"No external addresses found for family: {family}"
+
+    for addr in addrs:
+        url = (
+            f"http://{addr}:8888/api"
+            if family == socket.AF_INET
+            else f"http://[{addr}]:8888/api"
+        )
+        r = requests.get(url)
+        r.raise_for_status()
+        assert "version" in r.json()
+        print(f"Successfully connected to: {url}")
+
+
 def test_connect() -> None:
     make_get_request()
 
-    # https://docs.python.org/3/library/socket.html#socket.getaddrinfo
-    ipv4_addrs = {
-        s[4][0] for s in socket.getaddrinfo(socket.gethostname(), None, socket.AF_INET)
-    }
-    ipv4_addrs.discard("127.0.0.1")
-    if len(ipv4_addrs) < 1:
-        raise Exception("No external IPv4 addresses found")
-    for addr in ipv4_addrs:
-        url = f"http://{addr}:8888/api"
-        r = requests.get(url)
-        r.raise_for_status()
-        assert "version" in r.json()
-        print(f"Successfully connected to IPv4 {url}")
-
-    ipv6_addrs = {
-        s[4][0] for s in socket.getaddrinfo(socket.gethostname(), None, socket.AF_INET6)
-    }
-    ipv6_addrs.discard("::1")
-    if len(ipv6_addrs) < 1:
-        raise Exception("No external IPv6 addresses found")
-    for addr in ipv6_addrs:
-        url = f"http://[{addr}]:8888/api"
-        r = requests.get(url)
-        r.raise_for_status()
-        assert "version" in r.json()
-        print(f"Successfully connected to IPv6 {url}")
+    check_addrs(socket.AF_INET)
+    check_addrs(socket.AF_INET6)
 
 
 if __name__ == "__main__":
