d03069ac28b142ad3d30ac2bcaaac8bec526a153
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-03-20 15:50:10 +0000

    Always use docker.from_env

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,10 +28,7 @@ def http_client() -> requests.Session:
 @pytest.fixture(scope="session")
 def docker_client() -> docker.DockerClient:
     """Docker client configured based on the host environment"""
-    if "DOCKER_SOCK" in os.environ:
-        client = docker.DockerClient(base_url=os.environ["DOCKER_SOCK"])
-    else:
-        client = docker.from_env()
+    client = docker.from_env()
     LOGGER.info(f"Docker client created: {client.version()}")
     return client
 
