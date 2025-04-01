4d8e75d9be940b5b91327eefd13c8702cb3178c5
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-04-01 14:35:15 +0100

    Apply and merge tags in the same place

--- tagging/apps/merge_tags.py
+++ tagging/apps/merge_tags.py
@@ -16,54 +16,65 @@ docker = plumbum.local["docker"]
 LOGGER = logging.getLogger(__name__)
 
 
-def read_tags_from_files(config: Config) -> set[str]:
+def read_tags_from_files(config: Config) -> tuple[set[str], set[str]]:
     LOGGER.info(f"Read tags from file(s) for image: {config.image}")
 
-    tags: set[str] = set()
+    merged_tags: set[str] = set()
+    all_tags: set[str] = set()
     for platform in ALL_PLATFORMS:
         LOGGER.info(f"Reading tags for platform: {platform}")
 
         file_prefix = get_file_prefix_for_platform(platform, config.variant)
         filename = f"{file_prefix}-{config.image}.txt"
         path = config.tags_dir / filename
-        if path.exists():
-            LOGGER.info(f"Tag file: {path} found")
-            lines = path.read_text().splitlines()
-            tags.update(tag.replace(platform + "-", "") for tag in lines)
-        else:
+        if not path.exists():
             LOGGER.info(f"Tag file: {path} doesn't exist")
+            continue
+
+        LOGGER.info(f"Tag file: {path} found")
+        lines = path.read_text().splitlines()
+        for tag in lines:
+            all_tags.add(tag)
+            merged_tags.add(tag.replace(platform + "-", ""))
 
     LOGGER.info(f"Tags read for image: {config.image}")
-    return tags
+    return merged_tags, all_tags
 
 
-def merge_tags(tag: str, push_to_registry: bool) -> None:
+def merge_tags(tag: str, all_tags: set[str], push_to_registry: bool) -> None:
     LOGGER.info(f"Trying to merge tag: {tag}")
-    all_platform_tags = []
+
+    existing_platform_tags = []
     for platform in ALL_PLATFORMS:
         platform_tag = tag.replace(":", f":{platform}-")
-        LOGGER.info(f"Trying to pull: {platform_tag}")
+        if platform_tag in all_tags:
+            LOGGER.info(f"Tag {platform_tag} already exists, not pulling it")
+            existing_platform_tags.append(platform_tag)
+            continue
+        LOGGER.warning(f"Trying to pull: {platform_tag}")
         try:
             docker["pull", platform_tag] & plumbum.FG
-            all_platform_tags.append(platform_tag)
+            existing_platform_tags.append(platform_tag)
             LOGGER.info("Pull success")
         except plumbum.ProcessExecutionError:
-            LOGGER.info("Pull failed, image with this tag and platform doesn't exist")
+            LOGGER.warning(
+                "Pull failed, image with this tag and platform doesn't exist"
+            )
 
-    LOGGER.info(f"Found images: {all_platform_tags}")
+    LOGGER.info(f"Found images: {existing_platform_tags}")
+
+    # This allows to rerun the script without having to remove the manifest manually
     try:
         docker["manifest", "rm", tag] & plumbum.FG
         LOGGER.info(f"Manifest {tag} already exists, removing it")
     except plumbum.ProcessExecutionError:
         LOGGER.info(f"Manifest {tag} doesn't exist")
 
-    if push_to_registry:
-        # We need images to have been already pushed to the registry
-        # before creating the manifest
-        LOGGER.info(f"Creating manifest for tag: {tag}")
-        docker["manifest", "create", tag][all_platform_tags] & plumbum.FG
-        LOGGER.info(f"Successfully created manifest for tag: {tag}")
+    LOGGER.info(f"Creating manifest for tag: {tag}")
+    docker["manifest", "create", tag][existing_platform_tags] & plumbum.FG
+    LOGGER.info(f"Successfully created manifest for tag: {tag}")
 
+    if push_to_registry:
         LOGGER.info(f"Pushing manifest for tag: {tag}")
         docker["manifest", "push", tag] & plumbum.FG
         LOGGER.info(f"Successfully merged and pushed tag: {tag}")
@@ -78,7 +89,7 @@ if __name__ == "__main__":
     push_to_registry = os.environ.get("PUSH_TO_REGISTRY", "false").lower() == "true"
 
     LOGGER.info(f"Merging tags for image: {config.image}")
-    all_tags = read_tags_from_files(config)
-    for tag in all_tags:
-        merge_tags(tag, push_to_registry)
+    merged_tags, all_tags = read_tags_from_files(config)
+    for tag in merged_tags:
+        merge_tags(tag, all_tags, push_to_registry)
     LOGGER.info(f"Successfully merged tags for image: {config.image}")
