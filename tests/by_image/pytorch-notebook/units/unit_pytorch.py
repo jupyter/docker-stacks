fd7f3c2a8c3ffdec7291051682031603ca70a1a4
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-04-25 00:11:09 +0100

    Improve pytorch unit test

--- tests/by_image/pytorch-notebook/units/unit_pytorch.py
+++ tests/by_image/pytorch-notebook/units/unit_pytorch.py
@@ -3,3 +3,10 @@
 import torch
 
 print(torch.tensor([[1.0, 4.0, 7.0], [4.0, 9.0, 11.0]]))
+
+# Check if GPU is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+# Create large tensors
+size = 10000
+torch.randn(size, size, device=device)
