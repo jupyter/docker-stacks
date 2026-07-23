39c49d7161df82189000e73a0a14c8ebf52e3f1e
Ayaz Salikhov <mathbunnyru@gmail.com>
2026-07-23 12:52:31 +0100

    Replace skipped labextension test with a JupyterLab 4 check

--- tests/by_image/scipy-notebook/test_extensions.py
+++ tests/by_image/scipy-notebook/test_extensions.py
@@ -9,25 +9,39 @@ from tests.utils.tracked_container import TrackedContainer
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.mark.skip(reason="Not yet compliant with JupyterLab 4")
 @pytest.mark.parametrize(
     "extension",
     [
-        "@bokeh/jupyter_bokeh",
+        # Provided by the ipywidgets package
         "@jupyter-widgets/jupyterlab-manager",
+        # Provided by the jupyterlab-git package
+        "@jupyterlab/git",
+        # Provided by the ipympl package
         "jupyter-matplotlib",
     ],
 )
 def test_check_extension(container: TrackedContainer, extension: str) -> None:
-    """Basic check of each extension
+    """Check that the JupyterLab extensions shipped in the image
+    are installed and enabled.
 
-    The list of installed extensions can be obtained through this command:
+    The list of installed extensions is reported (on stderr) by:
 
     $ jupyter labextension list
 
     """
     LOGGER.info(f"Checking the extension: {extension} ...")
-    container.run_and_wait(
-        timeout=10,
-        command=["jupyter", "labextension", "check", extension],
+    _, stderr = container.run_and_wait(
+        timeout=30,
+        command=["jupyter", "labextension", "list"],
+        split_stderr=True,
     )
+    # The output is colorized even without a tty, e.g. "enabled" is printed as "\x1b[32menabled\x1b[0m"
+    stderr = TrackedContainer._strip_ansi(stderr)
+    extension_lines = [
+        line for line in stderr.splitlines() if line.split()[:1] == [extension]
+    ]
+    assert extension_lines, f"Extension {extension} is not installed:\n{stderr}"
+    for line in extension_lines:
+        words = line.split()
+        assert "enabled" in words, f"Extension {extension} is not enabled: {line}"
+        assert "OK" in words, f"Extension {extension} is not OK: {line}"
